852da451685b800811e745d20a68284c75565ffa
laltman12 <35745737+laltman12@users.noreply.github.com>
2018-01-25 16:36:09 -0600

    return and numdigits fixed
    Fixed numberOfDigits and Return false

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -1,4 +1,5 @@
 #include<math.h>
+#include<stdlib.h>
 
 #include "kaprekarUtils.h"
 
@@ -25,19 +26,19 @@
 int isKaprekar(int n) {
 
   if(n < 1) {
-    return false;
+    return 0;
   }
 
   int i;
   long square = n * (long) n;
-  int numDigits = (int) log10(n) + 1;
+  int numberOfDigits = (int) log10(n) + 1;
   long modulus = 0;
   long first, second;
 
   //for each possible "split" of the square...
   for(i=1; i<=numberOfDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
